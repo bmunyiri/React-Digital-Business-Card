a5f0e4f00a7cfc4934f89f6f6a27c6f38caf6ed3
bernard n munyiri <bernard.munyiri@gmail.com>
2022-06-04 15:57:47 +0300

    Changes to Portfolio Links Changes to Portfolio Links

--- src/ReactData.js
+++ src/ReactData.js
@@ -2,23 +2,37 @@ export default [
   {
     id: 1,
     name: "Digital Business Card",
-    link:
-      "https://codesandbox.io/s/digital-business-card-2l1w4j?file=/src/Experience.js"
+    link: (
+      <a href="https://codesandbox.io/s/digital-business-card-2l1w4j?file=/src/Experience.js">
+        Visit - Digital Business Card
+      </a>
+    )
   },
   {
     id: 2,
     name: "Scrimba React Course Project 1",
-    link: "https://codesandbox.io/s/iim1xh?file=/src/index.js"
+    link: (
+      <a href="https://codesandbox.io/s/iim1xh?file=/src/index.js">
+        Visit - React facts site
+      </a>
+    )
   },
   {
     id: 3,
     name: "Data Mapping - Most Expensive Cars In The World",
-    link: "https://codesandbox.io/s/v1j2v2?file=/src/App.js"
-  }
-  ,
+    link: (
+      <a href="https://codesandbox.io/s/v1j2v2?file=/src/App.js">
+        Visit - Most Expensive Cars In The World
+      </a>
+    )
+  },
   {
     id: 4,
     name: "Use State - Number Of People In The Room-Bulma For CSS",
-    link: "https://codesandbox.io/s/use-state-number-of-people-in-the-room-bulma-for-css-14gvb3"
+    link: (
+      <a href="https://codesandbox.io/s/use-state-number-of-people-in-the-room-bulma-for-css-14gvb3">
+        Visit - Number Of People In The Room
+      </a>
+    )
   }
 ];
--- src/WordPressData.js
+++ src/WordPressData.js
@@ -4,43 +4,63 @@ export default [
     name: "Kenya Web Professionals",
     link: (
       <a href="https://kenyawebprofessionals.com">
-        Visit Kenya Web Professionals .com!
+        Visit - Kenya Web Professionals.com
       </a>
     )
   },
   {
     id: 2,
     name: "iNFiNiTY Baisikeli Ltd",
-    link: "https://infinity.ke"
+    link: <a href="https://infinity.ke">Visit - iNFiNiTY Baisikeli Ltd.com</a>
   },
   {
     id: 3,
     name: "My lil Store Kenya",
-    link: "http://mylilstorekenya.com"
+    link: (
+      <a href="http://mylilstorekenya.com">Visit - My lil Store Kenya.com</a>
+    )
   },
   {
     id: 4,
     name: "Synergy International ICCT",
-    link: "http://www.synergyicct.org"
+    link: (
+      <a href="http://www.synergyicct.org">
+        Visit - Synergy International ICCT.com
+      </a>
+    )
   },
   {
     id: 5,
     name: "Manufacturers & Suppliers (K) Ltd",
-    link: "https://mslabrasives.com"
+    link: (
+      <a href="https://mslabrasives.com">
+        Visit - MANUFACTURERS & SUPPLIERS (K) LTD.com
+      </a>
+    )
   },
   {
     id: 6,
     name: "MacDavidson Consulting Group Limited",
-    link: "http://www.macdavidson.co.ke"
+    link: (
+      <a href="http://www.macdavidson.co.ke">
+        Visit - MacDavidson Consulting Group Limited (MCGL).com
+      </a>
+    )
   },
   {
     id: 7,
     name: "Safes and Office Security Ltd]",
-    link: "http://safesandofficesecurity.com"
+    link: (
+      <a href="http://safesandofficesecurity.com">
+        Visit - Safes and Office Security Ltd.com
+      </a>
+    )
   },
   {
     id: 8,
     name: "Stoic The Award-Winning Company",
-    link: "http://stoiccarcentre.com"
+    link: (
+      <a href="http://stoiccarcentre.com">Visit - Stoic Company Limited.com</a>
+    )
   }
 ];
